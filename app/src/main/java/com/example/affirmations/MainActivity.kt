2fac4dd9cdc72e311369e8f428eb31ae7a82774f
shruti.pahuja <shruti.pahuja@accenture.com>
2020-09-27 04:41:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val myDataSet = Datasource().loadAffirmations()
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
